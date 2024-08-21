731f3bf803a3ec2170f7fb739c78d5516d44e8df
rpanok <antti-jussi.oksa@tuni.fi>
2024-08-21 12:35:46 +0300

    Lisäsin while loop

--- Kayttoliittyma.cs
+++ Kayttoliittyma.cs
@@ -7,39 +7,42 @@ public class Kayttoliittyma
 {
     public static void Run(string[] args) // Renamed method to Run
     {
-        if (args.Length == 0)
+        while (true)
         {
-            Console.WriteLine("Ohjelma käynnistettiin ilman komentoriviparametreja.");
-        }
-        else
-        {
-            if (args[0] == "x")
+            if (args.Length == 0)
             {
-                Console.WriteLine("Ohjelma lopetetaan.");
-                return;
+                Console.WriteLine("Ohjelma käynnistettiin ilman komentoriviparametreja.");
             }
-            else if (args[0] == "-help" || args[0] == "-h")
+            else
             {
-                Console.WriteLine("Apu");
-                Console.WriteLine("p: Pelaa");
-                Console.WriteLine("t: Pisteet");
-                Console.WriteLine("-readfile: Lue tiedosto");
-                Console.WriteLine("x: Lopeta ohjelma.");
-                Console.WriteLine("quit, exit, q, x: Lopeta ohjelma.");
-            }
-            else if (args[0] == "p")
-            {
-                Console.WriteLine("Peli alkaa.");
-            }
-            else if (args[0] == "t")
-            {
-                Console.WriteLine("Pisteet");
-            }
-            // Remove the -readfile case since FileReader is no longer used
-            else if (args[0] == "hyvaksynta")
-            {
-                Hyvaksynta2 hyvaksynta = new Hyvaksynta2();
-                hyvaksynta.Run();
+                if (args[0] == "x")
+                {
+                    Console.WriteLine("Ohjelma lopetetaan.");
+                    return;
+                }
+                else if (args[0] == "-help" || args[0] == "-h")
+                {
+                    Console.WriteLine("Apu");
+                    Console.WriteLine("p: Pelaa");
+                    Console.WriteLine("t: Pisteet");
+                    Console.WriteLine("-readfile: Lue tiedosto");
+                    Console.WriteLine("x: Lopeta ohjelma.");
+                    Console.WriteLine("quit, exit, q, x: Lopeta ohjelma.");
+                }
+                else if (args[0] == "p")
+                {
+                    Console.WriteLine("Peli alkaa.");
+                }
+                else if (args[0] == "t")
+                {
+                    Console.WriteLine("Pisteet");
+                }
+                // Remove the -readfile case since FileReader is no longer used
+                else if (args[0] == "hyvaksynta")
+                {
+                    Hyvaksynta2 hyvaksynta = new Hyvaksynta2();
+                    hyvaksynta.Run();
+                }
             }
         }
     }
