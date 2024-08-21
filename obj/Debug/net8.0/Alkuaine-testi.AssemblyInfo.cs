d78071220ce19de26c1a919bbc68aa3de829c65a
DeaAlexandra <alexandra.saarelainen@elisanet.fi>
2024-08-21 19:08:17 +0300

    testi

--- obj/Debug/net8.0/Alkuaine-testi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Alkuaine-testi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alkuaine-testi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6205590047a538c7834e0da36c72879543adea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ad2ac0bc861ae28d39d5830a69ef0d7e9e024c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alkuaine-testi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alkuaine-testi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
